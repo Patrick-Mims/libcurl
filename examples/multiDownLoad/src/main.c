05b9a6cebad84a0710f14cf2c90b761a206055b3
Patrick Mims <patrickkevinmims@gmail.com>
2021-08-19 18:57:14 -0700

    project init

---
+++ examples/multiDownLoad/src/main.c
@@ -0,0 +1,37 @@
+#include <curl/curl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+/* An array whose elements are pointers to strings */
+
+const char *urls[] =
+{
+  "microsoft",
+  "oracle"
+};
+
+static void addTransfer(CURLM *message, int i)
+{
+
+}
+
+int main(void)
+{
+  int transfer = 0;
+  CURLM *curl_multi;
+  CURLMsg *message;
+
+  curl_global_init(CURL_GLOBAL_ALL);
+  message = curl_multi_init();
+
+  /* limit the amount of simultaneous connections curl should allow */
+  curl_multi_setopt(message, CURLMOPT_MAXCONNECTS, 10L);
+
+  for(transfer; transfer < 10; ++transfer) {
+    addTransfer(message, transfer);
+  }
+
+  return 0;
+}
