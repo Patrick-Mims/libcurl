2994707e325fcd8e49a2a1b9d633f89f13fdf387
Patrick Mims <patrickkevinmims@gmail.com>
2021-08-20 19:41:15 -0700

    cleaning up transfer(CURLM *, const4 char **)

--- examples/multiDownLoad/src/main.c
+++ examples/multiDownLoad/src/main.c
@@ -4,23 +4,37 @@
 #include <string.h>
 #include <unistd.h>
 
-#define SIZE 10
+#define SIZE 5
 
-static void transfer(CURLM *msg, const char *l[])
+static size_t callback()
+{
+}
+
+static void transfer(CURLM *msg, const char **l)
 {
   CURL *eh = curl_easy_init();
   int count = 0;
 
+  /*
   l[0] = "https://www.microsoft.com";
   l[1] = "https://www.apple.com";
   l[2] = "https://www.oracle.com";
   l[3] = "https://www.amazon.com";
   l[4] = "https://www.cisco.com";
-  l[5] = "https://www.ibm.com";
+  */
 
-  for (count; count < SIZE; ++count)
+  for (count; count <= SIZE; count++)
   {
-    curl_easy_setopt(eh, CURLOPT_URL, l[count]);
+    curl_easy_setopt(eh, CURLOPT_VERBOSE, 1);
+    //curl_easy_setopt(eh, CURLOPT_WRITEFUNCTION, callback);
+    curl_easy_setopt(eh, CURLOPT_URL, *(l + count));
+    //curl_easy_setopt(eh, CURLOPT_URL, *(l + count));
+    //printf("=>%s\n", *(l + count));
+    //curl_easy_setopt(eh, CURLOPT_URL, l[count]);
+    /*
+    curl_easy_setopt(eh, CURLOPT_WRITEFUNCTION, write_cb);
+    curl_easy_setopt(eh, CURLOPT_PRIVATE, l[count]);
+    */
     curl_multi_add_handle(msg, eh);
   }
 
