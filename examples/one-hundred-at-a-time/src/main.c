a2f23b1e4b452bc5ac761d4776f9ae59d41103ff
Patrick Mims <patrickkevinmims@gmail.com>
2022-01-24 18:17:30 -0800

    implemented function curl_easy()

--- examples/one-hundred-at-a-time/src/main.c
+++ examples/one-hundred-at-a-time/src/main.c
@@ -1,9 +1,9 @@
-#include <stdio.h>
 #include <curl/curl.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#define SIZE 30
+#define SIZE 50
 #define NUM_URLS
 
 struct node
@@ -19,7 +19,18 @@ static size_t callback(char *data, size_t n, size_t l, void *userp)
 }
 */
 
-static void display(struct node *list)
+/*
+void display(struct node *list)
+{
+  struct node *p = NULL;
+
+  for (p = list; p != NULL; p = p->next)
+  {
+    printf("%s\n", p->url);
+  }
+}
+
+d display(struct node *list)
 {
   struct node *s = NULL;
 
@@ -28,16 +39,37 @@ static void display(struct node *list)
     printf("%s\n", s->url);
   }
 }
+*/
 
-static struct node insert_node(struct node **list, char *item)
+void curl_easy(CURL *curl, char *str)
+{
+
+  if(curl == NULL)
+  {
+    printf("something is wrong with curl_easy()\n");
+    exit(EXIT_FAILURE);
+  }
+
+  printf("URL: %s\n", str);
+
+  curl_easy_setopt(curl, CURLOPT_URL, str);
+  curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+
+  if((curl_easy_perform(curl)) == CURLE_OK)
+  {
+    printf("SUCCESS!\n");
+  }
+
+}
+
+void insert_node(struct node **list, char *item)
 {
   struct node *new_node = NULL;
 
   if((new_node = malloc(sizeof(struct node))) == NULL)
-  {
     exit(EXIT_FAILURE);
-  }
 
+  printf("Pushing -> %s\n", item);
   strcpy(new_node->url, item);
 
   new_node->next = *list;
@@ -46,10 +78,12 @@ static struct node insert_node(struct node **list, char *item)
 
 int main(int argc, char **argv)
 {
-  CURLM *multi_curl;
-  CURLMsg *multi_message;
+  CURL *curl;
+  //CURLcode ret;
+  //CURLMsg *multi_message;
 
-  curl_global_init(CURL_GLOBAL_DEFAULT);
+  curl = curl_easy_init();
+  //curl_global_init(CURL_GLOBAL_DEFAULT);
 
   int count = 0;
   char item[SIZE];
@@ -78,9 +112,12 @@ int main(int argc, char **argv)
 
   fclose(fp);
 
-  display(head);
+  //display(head);
+  //gcc -Wall -o build/main7 src/main.c -lcurl -I~/source-code/curl/include
 
   printf("count: %d\n", count);
+
+  curl_easy(curl, "https://www.microsoft.com");
 
   return 0;
 }
